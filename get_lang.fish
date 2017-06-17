228e92ec6e7894cf7de63c7ddba9705036e44efd
Vincent Truchseß <redtux@posteo.net>
2017-06-17 15:43:49 +0200

    added symbols for java and fish

--- get_lang.fish
+++ get_lang.fish
@@ -1,8 +1,12 @@
 function get_lang
   set -e symbol
-  for file in (ls)
+  for file in (find . *)
     if string match -r ".*\.hs" $file > /dev/null
       set symbol \u03BB
+    else if string match -r ".*\.java" $file > /dev/null
+      set symbol \u2615
+    else if string match -r ".*\.fish" $file > /dev/null
+      set symbol "><(((*> "
     end
   end
   echo $symbol
